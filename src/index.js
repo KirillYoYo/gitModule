02578c7a5571b761c7318dfb47959e03eb2b041b
kmyasnikov <K.Myasnikov@ase-ec.ru>
2023-03-22 14:12:27 +0300

    base

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+const { simpleGit, CleanOptions } = require('simple-git');
+
+const options = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+  trimmed: false,
+};
+
+console.log('options', options)
+
+const git = simpleGit(options);
+
+console.log('----', )
 console.log('start')
+git.pull()
+// git.clean(simpleGit.CleanOptions.DRY_RUN);
+// git.clean(CleanOptions.FORCE);
+git.diff()
+console.log('git.diff()', git.diff())
+git.commit('message', (args) => console.log('args', args))
